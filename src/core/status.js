64391578db27b4025ed564e8ec6f3ab292117cbd
Edward Li <vk.edward.li@gmail.com>
2020-10-31 00:19:57 +0800

    Change online status

--- src/core/status.js
+++ src/core/status.js
@@ -11,7 +11,7 @@ module.exports = function(bot, status, channel, writable = true)
          bot.setPresence({
             status: "online",
             game: {
-               name: "!t help / !translate help"
+               name: "機動戦士ガンダム 連邦vs.ジオンDX"
             }
          });
       },
